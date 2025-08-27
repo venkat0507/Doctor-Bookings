fcc2465f48cc05f019ab0d568b88db7a4081507f
venkat0507 <154009712+venkat0507@users.noreply.github.com>
2025-08-27 14:28:32 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5174 }
+  server: { port: 5173 }
   
 })
