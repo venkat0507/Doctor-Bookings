4b98ae7c170dde964465fda1e2c177cb9ee349ee
venkat0507 <154009712+venkat0507@users.noreply.github.com>
2025-08-27 14:24:41 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#5f6FFF",
+      },
+      gridTemplateColumns: {
+        auto: "repeat(auto-fill, minmax(200px, 1fr))",
+      },
+    },
+  },
+};
